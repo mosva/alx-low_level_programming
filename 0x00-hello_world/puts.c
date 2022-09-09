703f1e40bc5716b6fa66983e310e0365e1dcfab4
mosva <mostafaalaa@live.com>
2022-09-08 19:40:10 -0700

    AS

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-puts.c
+#include <stdio.h>
+/**
+ *
+ *main - Entry point
+ *
+ *
+ *
+ *Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
