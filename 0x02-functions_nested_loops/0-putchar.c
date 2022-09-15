a837f6b6d4a5f999f95ac92bea6b3351ad2c8cb5
mosva <mostafaalaa@live.com>
2022-09-14 18:13:18 -0700

    sa

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
