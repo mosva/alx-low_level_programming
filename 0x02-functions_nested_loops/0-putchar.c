f3125a10ed0406de2db08c502ca2b15e115897fe
mosva <mostafaalaa@live.com>
2022-09-14 18:29:31 -0700

    As

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
+
 
 
 /**
@@ -10,7 +10,16 @@
 
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
+
+}
