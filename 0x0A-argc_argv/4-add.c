9f7677d5759ec98f13a574fb665f36858f452043
mosva <mostafaalaa@live.com>
2022-09-30 19:36:55 -0700

    As

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * main - Entry Point
- * @argc: arguments
- * @argv: array pointing to arguments
- * Return: 0
+ * main - add positive numbers.
+ * @argc: count arguments passed in argv
+ * @argv: String that holds the passed Arguments.
+ *
+ * Return: 0 (Sucess) else 1 (Failure).
  */
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, c, sum = 0;
 
-	if (argc < 1)
+	if (argc < 2)
+	{
+		printf("0\n");
 		return (0);
+	}
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		for (c = 0; argv[i][c] != '\0'; c++)
 		{
-			printf("%s\n", "Error");
-			return (1);
+			if (!isdigit(argv[i][c]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
 }
+
